09c094192de76c81500c506c3ce5dffce9a1c7f1
Tywayne <tywaynebuchanan@gmail.com>
2020-05-15 18:27:44 -0500

    changes made

--- www/script.js
+++ www/script.js
@@ -8,34 +8,27 @@ var USDYEN= rates[4];
 var USDCY= rates[5];
 
 
-function Calculate() {
+function ChangeCurrency2() {
+	var curr = document.getElementById("select_one").value;
+	if (curr == 'USD'){
+		document.getElementById('input_one').value = rates[0];
+	}
+	if (curr == 'JMD'){
+		document.getElementById('input_one').value = rates[1];
+	}
+	if (curr == 'GDP'){
+		document.getElementById('input_one').value = rates[2];
+	}
+	if (curr == 'CDN'){
+		document.getElementById('input_one').value = rates[3];
+	}
+	if (curr == 'YEN'){
+		document.getElementById('input_one').value = rates[4];
+	}
+	if (curr == 'CY'){
+		document.getElementById('input_one').value = rates[5];
+	}
 
-	var TotalOne = document.getElementById("input_one").value;
-	// var TotalTwo = document.getElementById("input_two").value;
-	var currency = document.getElementById("select_one").value;
-	var currency_2 = document.getElementById("select_two").value;
-	var Rate = 0; 
-	var Total = 0;
-
-
-if (currency == currency_2 ){
-	Total = TotalOne * USDUSD;
-} else if (currency == 'USD' && currency_2 == 'JMD'){
-	Total = TotalOne * USDJMD;
-} else if (currency == 'USD' && currency_2 == 'YEN'){
-	Total = TotalOne * USDYEN;
-} else if (currency == 'USD' && currency_2 == 'GDP'){
-	Total = TotalOne * USDGDP;
-} else if (currency == 'USD' && currency_2 == 'CDN'){
-	Total = TotalOne * USDCDN;
-}else if (currency == 'USD' && currency_2 == 'CY'){
-	Total = TotalOne * USDCY;
-}
-
-	document.getElementById("input_two").value = Total;
-	document.getElementById("label1").innerHTML = TotalOne + " " + currency + " = " + Total + currency_2;
-
-	
 }
 
 function Swap(){
@@ -55,6 +48,59 @@ function Swap(){
 	input_one.value = input_two.value;
 	input_two.value = temp;
 	
+
+}
+
+
+
+function ChangeCurrency(){
+
+	var TotalOne = document.getElementById("input_one").value;
+	var currency = document.getElementById("select_one").value;
+	var currency_2 = document.getElementById("select_two").value;
+	var Rate = 0; 
+	var Total = 0;
+
+	var curr = document.getElementById("select_two").value;
+	if (curr == 'USD'){
+		document.getElementById('input_two').value = rates[0];
+	}
+	if (curr == 'JMD'){
+		document.getElementById('input_two').value = rates[1];
+	}
+	if (curr == 'GDP'){
+		document.getElementById('input_two').value = rates[2];
+	}
+	if (curr == 'CDN'){
+		document.getElementById('input_two').value = rates[3];
+	}
+	if (curr == 'YEN'){
+		document.getElementById('input_two').value = rates[4];
+	}
+	if (curr == 'CY'){
+		document.getElementById('input_two').value = rates[5];
+	}
+
+
+	
+
+
+if (currency == currency_2 ){
+	Total = TotalOne * USDUSD;
+} else if (currency == 'USD' && currency_2 == 'JMD'){
+	Total = TotalOne * USDJMD;
+} else if (currency == 'USD' && currency_2 == 'YEN'){
+	Total = TotalOne * USDYEN;
+} else if (currency == 'USD' && currency_2 == 'GDP'){
+	Total = TotalOne * USDGDP;
+} else if (currency == 'USD' && currency_2 == 'CDN'){
+	Total = TotalOne * USDCDN;
+}else if (currency == 'USD' && currency_2 == 'CY'){
+	Total = TotalOne * USDCY;
+}
+
+	document.getElementById("input_two").value = Total;
+	document.getElementById("label1").innerHTML = TotalOne + " " + currency + " = " + Total.toFixed(2) + currency_2;
 
 }
 
